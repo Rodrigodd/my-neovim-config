74aab9b104f8d598de0f72ca332f0a6b6b86cc39
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-06-30 22:34:32 -0300

    add options for verilog

---
+++ ftplugin/verilog.lua
@@ -0,0 +1,4 @@
+vim.o.tabstop = 4 -- number of visual spaces per TAB
+vim.o.softtabstop = 4 -- number of spaces in tab when editing
+vim.o.shiftwidth = 4 -- number of spaces to use for autoindent
+vim.o.expandtab = false -- tabs are space
