fdb99115311e9e5d7eb0d0fb0e8f879081ea1586
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2022-10-30 17:37:33 -0300

    add nvim-surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -32,6 +32,15 @@ require('packer').startup({ function()
         end,
     }
     use {
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
+    use {
         'windwp/nvim-autopairs',
         config = function()
             local npairs = require('nvim-autopairs');
