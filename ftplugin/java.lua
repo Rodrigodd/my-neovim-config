a08ec678e8dc9e5011f29ea365ade41d12a4587d
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2023-01-03 16:27:17 -0300

    fix java config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,19 +1,52 @@
 local nvim_lsp = require('lspconfig')
 
+local jdtls_path = require('mason-registry').get_package('jdtls'):get_install_path()
+
+-- config_win, config_linux or config_mac
+local config_folder = '/config_win'
+local VERSION_NUMBER = '1.6.400.v20210924-0641';
+
+local root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' })
+
+local hash = string.sub(vim.fn.system('md5sum', root_dir), 1, 32)
+
+local data_path = ""
+if config_folder == '/config_win' then
+    data_path = [[C:\Users\Rodrigo\AppData\Local\Temp\jdtl_]] .. hash
+else
+    data_path = "/tmp/jdtl_" .. hash
+end
+
+local lombok_path = jdtls_path .. '/lombok.jar'
+
 -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
 local config = {
-    -- The command that starts the language server
-    -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-    cmd = { 'jdtls' },
+    -- cmd = { 'jdtls.cmd' },
 
-    -- 💀
-    -- This is the default if not provided, you can remove it. Or adjust as needed.
-    -- One dedicated LSP server & client will be started per unique root_dir
-    -- root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' }),
+    cmd = {
+        'java',
+        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+        '-Dosgi.bundles.defaultStartLevel=4',
+        '-Declipse.product=org.eclipse.jdt.ls.core.product',
+        '-Dlog.protocol=true',
+        '-Dlog.level=ALL',
+        '-Xms1g',
+        '--add-modules=ALL-SYSTEM',
+        '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+        '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
 
-    -- Here you can configure eclipse.jdt.ls specific settings
-    -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-    -- for a list of options
+        -- lombok
+        '-javaagent:' .. lombok_path,
+
+        '-jar', jdtls_path .. '/plugins/org.eclipse.equinox.launcher_' .. VERSION_NUMBER .. '.jar',
+
+        '-configuration', jdtls_path .. config_folder,
+
+        '-data', data_path
+    },
+
+    root_dir = root_dir,
+
     settings = {
         java = {
             -- project = {
@@ -28,28 +61,22 @@ local config = {
                 runtimes = {
                     {
                         name = "JavaSE-1.8",
-                        path = "C:/Program Files/RedHat/java-1.8.0-openjdk-1.8.0.265-3",
-                    }
+                        path = "C:\\Program Files\\RedHat\\java-1.8.0-openjdk-1.8.0.265-3"
+                    },
+                    {
+                        name = "JavaSE-19",
+                        path = "C:\\Users\\Rodrigo\\scoop\\apps\\openjdk\\current",
+                        sources = "C:\\Users\\Rodrigo\\scoop\\apps\\openjdk\\current\\lib\\src.zip",
+                        default = true,
+                    },
                 }
             }
         }
     },
 
-    -- Language server `initializationOptions`
-    -- You need to extend the `bundles` with paths to jar files
-    -- if you want to use additional eclipse.jdt.ls plugins.
-    --
-    -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-    --
-    -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-    init_options = {
-        bundles = {}
-    },
 
-    capabilities = nvim_lsp.util.default_config.capabilities,
+    -- capabilities = nvim_lsp.util.default_config.capabilities,
     on_attach = nvim_lsp.util.default_config.on_attach,
 }
 
--- This starts a new client & server,
--- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach({ cmd = { 'jdtls.cmd' } })
+require('jdtls').start_or_attach(config)
