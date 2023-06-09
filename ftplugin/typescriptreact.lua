7ad6d7b3454fc1b0964b9f1a27e7709ee8c290a4
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2023-06-09 16:54:56 -0300

    set tabsize=2 for typescript

---
+++ ftplugin/typescriptreact.lua
@@ -0,0 +1,3 @@
+vim.opt_local.tabstop = 2 -- number of visual spaces per TAB
+vim.opt_local.softtabstop = 2 -- number of spaces in tab when editing
+vim.opt_local.shiftwidth = 2 -- number of spaces to use for autoindent
