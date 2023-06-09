0d3f8646edb9cb8b2ae4c345dd2a80fed3423707
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2023-06-09 16:58:45 -0300

    install ssr.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,6 +18,24 @@ return {
         end,
     },
     {
+        'cshuaimin/ssr.nvim',
+        config = function()
+            require("ssr").setup {
+                min_width = 50,
+                min_height = 5,
+                max_width = 120,
+                max_height = 25,
+                keymaps = {
+                    close = "q",
+                    next_match = "n",
+                    prev_match = "N",
+                    replace_confirm = "<cr>",
+                    replace_all = "<leader><cr>",
+                },
+            }
+            vim.keymap.set({ "n", "x" }, "<leader>sr", function() require("ssr").open() end)
+        end
+    },
         'nmac427/guess-indent.nvim',
         config = function()
             require('guess-indent').setup { auto_cmd = false }
