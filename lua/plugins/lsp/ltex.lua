084dab567be030cd7fb85a23e55c8b9017289a2c
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2023-10-13 19:04:29 -0300

    fix vscode path in ltex config

--- lua/plugins/lsp/ltex.lua
+++ lua/plugins/lsp/ltex.lua
@@ -5,9 +5,9 @@ nvim_lsp.ltex.setup {
     on_attach = function(client, bufnr)
         require("ltex_extra").setup {
             load_langs = { "pt-Br", "en-US" }, -- table <string> : languages for witch dictionaries will be loaded
-            init_check = true, -- boolean : whether to load dictionaries on startup
-            path = '.vscode', -- string : path to store dictionaries. Relative path uses current working directory
-            log_level = "none", -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+            init_check = true,                 -- boolean : whether to load dictionaries on startup
+            path = '.vscode/',                 -- string : path to store dictionaries. Relative path uses current working directory
+            log_level = "none",                -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
         }
 
         require('lspconfig.util').default_config.on_attach(client, bufnr)
