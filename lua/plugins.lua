0e93f3c9c96e0ca46f8f93912a6d806b4229ef3e
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2024-01-09 16:51:37 -0300

    add 'Vonr/align.nvim'

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -72,6 +72,105 @@ return {
         end,
     },
     {
+        'Vonr/align.nvim',
+        branch = "v2",
+        lazy = true,
+        init = function()
+            -- Create a cmd for it
+            vim.api.nvim_create_user_command(
+            "Align",
+            function (opts)
+                require('align').align_to_string({
+                    preview = true,
+                    regex = false,
+                })
+            end,
+            { nargs = 0 }
+            )
+
+            local NS = { noremap = true, silent = true }
+
+            -- Aligns to 1 character
+            vim.keymap.set(
+            'x',
+            'aa',
+            function()
+                require'align'.align_to_char({
+                    length = 1,
+                })
+            end,
+            NS
+            )
+
+            -- Aligns to 2 characters with previews
+            vim.keymap.set(
+            'x',
+            'ad',
+            function()
+                require'align'.align_to_char({
+                    preview = true,
+                    length = 2,
+                })
+            end,
+            NS
+            )
+
+            -- Aligns to a string with previews
+            vim.keymap.set(
+            'x',
+            'aw',
+            function()
+                require'align'.align_to_string({
+                    preview = true,
+                    regex = false,
+                })
+            end,
+            NS
+            )
+
+            -- Aligns to a Vim regex with previews
+            vim.keymap.set(
+            'x',
+            'ar',
+            function()
+                require'align'.align_to_string({
+                    preview = true,
+                    regex = true,
+                })
+            end,
+            NS
+            )
+
+            -- Example gawip to align a paragraph to a string with previews
+            vim.keymap.set(
+            'n',
+            'gaw',
+            function()
+                local a = require'align'
+                a.operator(
+                a.align_to_string,
+                {
+                    regex = false,
+                    preview = true,
+                }
+                )
+            end,
+            NS
+            )
+
+            -- Example gaaip to align a paragraph to 1 character
+            vim.keymap.set(
+            'n',
+            'gaa',
+            function()
+                local a = require'align'
+                a.operator(a.align_to_char)
+            end,
+            NS
+            )
+        end
+    },
+    {
         'nmac427/guess-indent.nvim',
         config = function()
             require('guess-indent').setup { auto_cmd = false }
