a06a2096ae3d5717fba09168b301bb86536ed51c
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2024-04-18 14:53:31 -0300

    telescope: add fzf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -223,6 +223,10 @@ return {
             'nvim-lua/plenary.nvim',
             'nvim-telescope/telescope-ui-select.nvim',
             { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+            },
         },
         config = function()
             require('plugins.telescope')
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -34,11 +34,19 @@ telescope.setup {
             -- theme = "dropdown", -- use dropdown theme
             -- theme = { }, -- use own theme spec
             -- layout_config = { mirror=true }, -- mirror preview pane
+        },
+        fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
         }
     }
 }
 telescope.load_extension("ui-select")
 telescope.load_extension('live_grep_args')
+telescope.load_extension('fzf')
 
 local tsb = require 'telescope.builtin'
 vim.keymap.set('n', '<leader>fr', tsb.resume)
