014d82b28089e38def2770d178b74639a2644789
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2024-04-26 16:26:35 -0300

    Install rest.nvim
    Didn't work on Windows. Need to test on linux still.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -546,5 +546,32 @@ return {
             vim.g.mkdp_echo_preview_url = true
             vim.g.mkdp_page_title = '${name}'
         end,
+    },
+
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+        opts = {
+            rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+        }
+
+    },
+    {
+        "rest-nvim/rest.nvim",
+        ft = "http",
+        dependencies = { "luarocks.nvim" },
+        config = function()
+            require("rest-nvim").setup {
+                keybinds = {
+                    {
+                        "<localleader>rr", "<cmd>Rest run<cr>", "Run request under the cursor",
+                    },
+                    {
+                        "<localleader>rl", "<cmd>Rest run last<cr>", "Re-run latest request",
+                    },
+                }
+            }
+        end,
     }
 }
