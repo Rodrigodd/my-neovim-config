53fdc6f81c9557909b288f3f50ff0613ac8c8925
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2024-12-21 21:02:18 -0300

    fix: ignore unhelpful error from nvim with rust-analyzer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,16 @@ local nvim_lsp = require('lspconfig')
 local lsp_status = require('lsp-status')
 lsp_status.register_progress()
 
+-- See: https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
 
 local on_attach = function(client, bufnr)
     -- print("on attach " .. bufnr)
