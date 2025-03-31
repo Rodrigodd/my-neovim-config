4f470895843e54fb1178d7653e11ec7d2b064670
Rodrigodd <rodrigobatsmoraes@hotmail.com>
2025-03-31 19:29:16 -0300

    Increate telescope-undo ctxlen

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,7 +24,7 @@ telescope.setup {
         live_grep_args = {
             auto_quoting = true, -- enable/disable auto-quoting
             -- define mappings, e.g.
-            mappings = { -- extend mappings
+            mappings = {         -- extend mappings
                 i = {
                     ["<C-k>"] = lga_actions.quote_prompt(),
                     ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
@@ -36,14 +36,16 @@ telescope.setup {
             -- layout_config = { mirror=true }, -- mirror preview pane
         },
         fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+            fuzzy = true,                   -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true,    -- override the file sorter
+            case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
         },
         undo = {
-
+            vim_diff_opts = {
+                ctxlen = 5,
+            },
         }
     }
 }
